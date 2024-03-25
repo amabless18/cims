e2a54badd83b556f2da634f8f739d2906fb01f3c
Enrico <106855992+Jaykawaii20@users.noreply.github.com>
2024-03-26 00:47:47 +0800

    (chore) Student Dashboard Enhancement

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { DataService } from 'src/app/modules/service/data.service';
 import { Social } from 'src/app/social';
 import { SocialComponent } from '../social/social.component';
 import { CreateOrEditSocialComponent } from '../social/createoredit-social.component';
-import { startWith, switchMap } from 'rxjs/operators';
+import { map, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,7 +17,8 @@ export class DashboardComponent  implements OnInit {
   user$: Observable<UserType>;
   socials = new Social();
   social:any;
-
+  coachFullName: any;
+  
   constructor(
     private auth: AuthService,
     private dataService:DataService, 
@@ -29,7 +30,7 @@ export class DashboardComponent  implements OnInit {
   ngOnInit(): void {
     this.user$ = this.auth.currentUserSubject.asObservable();
     this.getAll();
-
+    this.getCoach();
     interval(5000)
       .pipe(
         startWith(0), // emit first value immediately
@@ -37,6 +38,7 @@ export class DashboardComponent  implements OnInit {
       )
       .subscribe((res) => {
         this.social = res;
+        //this.coachFullName = this.getCoachFullName(res.coach_id);
         this.cdr.detectChanges();
       });
   }
@@ -54,4 +56,34 @@ export class DashboardComponent  implements OnInit {
       this.cdr.detectChanges();
     });
   }
+
+  getCoach() {
+    this.user$.subscribe((_user) => {
+      if (_user && _user.coach_id) {
+        this.getCoachFullName(_user.coach_id).subscribe((coachFullName) => {
+          this.coachFullName = coachFullName;
+          //console.log('Coach Full Name:', this.coachFullName);
+          // Now you can do whatever you want with the coachFullName, like storing it in a variable to use in the template
+          this.cdr.detectChanges();
+        });
+      }
+    });
+  }
+
+  getCoachFullName(coachId: any): Observable<any> {
+    return this.dataService.getUserDatabyId(coachId).pipe(
+      tap((userData: any) => {
+        //console.log('User Data:', userData);
+      }),
+      map((userData: any) => {
+        if (userData && userData.firstname && userData.lastname) {
+          return userData.firstname + ' ' + userData.middlename + ' ' + userData.lastname;
+        } else {
+          return 'Coach'; // Default to 'Coach' if full name is not available
+        }
+      })
+    );
+  }
+  
+  
 }
