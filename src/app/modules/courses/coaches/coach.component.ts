a8a8292cd2fed0b47eca8f19303d348997e2b04f
Jaykawaii20 <106855992+Jaykawaii20@users.noreply.github.com>
2024-05-07 22:58:08 -0700

    (chore) Implement printer

--- src/app/modules/courses/coaches/coach.component.ts
+++ src/app/modules/courses/coaches/coach.component.ts
@@ -60,7 +60,75 @@ export class CoachComponent {
     });
   }
 
-
+  print() {
+    const printContentElement = document.getElementById("printContent");
+    if (printContentElement) {
+      const printContent = printContentElement.innerHTML;
+  
+      // Create a hidden iframe
+      const iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
+      document.body.appendChild(iframe);
+      const doc = iframe.contentWindow?.document || iframe.contentDocument;
+      
+      // Set iframe content
+      if (doc) {
+        doc.open();
+        doc.write(
+          `
+          <html>
+          <head>
+            <style>
+              @media print {
+                /* Include your print-specific CSS styles here */
+                table {
+                  width: 100%;
+                  border-collapse: collapse;
+                }
+                th, td {
+                  border: 1px solid black;
+                  padding: 8px;
+                  text-align: left;
+                }
+                th {
+                  background-color: #f2f2f2;
+                }
+                button, img, span {
+                  display: none;
+                }
+                .user-icon {
+                  display: none;
+                }
+                #printdisplay{
+                  display: none;
+                }
+              }
+            </style>
+          </head>
+          <body>
+            <div>
+              <div *ngIf="user$ | async as _user">
+                ${printContent}
+              </div>
+            </div>
+            
+          </body>
+        </html>
+      `
+        );
+        doc.close();
+  
+        // Wait for iframe to load before printing
+        iframe.onload = function() {
+          iframe.contentWindow?.focus();
+          iframe.contentWindow?.print();
+          document.body.removeChild(iframe);
+        };
+      }
+    } else {
+      console.error("Element with id 'printContent' not found.");
+    }
+  }
  
 
   openCreateOrEditModal() {
